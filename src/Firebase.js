11281e8c0ca4ccc8291a26a3f459ec8c8189ea25
Kien Vu <kienv91@gmail.com>
2019-02-07 16:42:34 +0100

    add: Firebase.js

---
+++ src/Firebase.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import app from 'firebase/app';
+import 'firebase/auth';
+import config from './config';
+
+export const FirebaseContext = React.createContext(null);
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+  }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
