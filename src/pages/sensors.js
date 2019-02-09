78efd7d8fb34bf08f59d990684785777e227f731
Kien Vu <kienv91@gmail.com>
2019-02-09 10:25:55 +0100

    refact: sensor.js

--- src/pages/sensors.js
+++ src/pages/sensors.js
@@ -1,50 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, Center } from '../elements';
 import { CircularProgress } from '@material-ui/core';
 
 import { withFirebase } from '../Firebase';
 import Sensor from '../components/Sensor';
 
-class Sensors extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: false,
-      measurements: {},
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ loading: true });
-
-    this.props.firebase.measurements().on('value', snapshot => {
-      this.setState({
-        measurements: snapshot.val(),
-        loading: false,
-      });
-    });
-  }
-
-  render() {
-    if (this.state.loading) {
-      return (
-        <Layout>
-          <Center>
-            <CircularProgress />
-          </Center>
-        </Layout>
+function Sensors({ firebase }) {
+  const [sensorData, setData] = useState({ loading: true, measurements: null });
+  useEffect(() => {
+    firebase
+      .measurements()
+      .on('value', snapshot =>
+        setData({ loading: false, measurements: snapshot.val() }),
       );
-    }
+  });
 
-    return (
-      <Layout>
-        {Object.keys(this.state.measurements).map(sensorId => (
-          <Sensor key={sensorId} id={sensorId} />
-        ))}
-      </Layout>
-    );
-  }
+  return sensorData.loading ? (
+    <Layout>
+      <Center>
+        <CircularProgress />
+      </Center>
+    </Layout>
+  ) : (
+    <Layout>
+      {Object.keys(sensorData.measurements).map(sensorId => (
+        <Sensor key={sensorId} id={sensorId} />
+      ))}
+    </Layout>
+  );
 }
 
 export default withFirebase(Sensors);
