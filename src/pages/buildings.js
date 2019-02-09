7d88596c832e680a86c90c19b969afe50893c78e
Kien Vu <kienv91@gmail.com>
2019-02-09 10:51:04 +0100

    refact: buildings.js

--- src/pages/buildings.js
+++ src/pages/buildings.js
@@ -1,50 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, Center } from '../elements';
 import { CircularProgress } from '@material-ui/core';
 
 import { withFirebase } from '../Firebase';
 import Building from '../components/Building';
 
-class Buildings extends React.Component {
-  constructor(props) {
-    super(props);
+function Buildings({ firebase }) {
+  const [buildingData, setData] = useState({ loading: true, buildings: null });
 
-    this.state = {
-      loading: false,
-      buildings: {},
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ loading: true });
-
-    this.props.firebase.buildings().on('value', snapshot => {
-      this.setState({
-        buildings: snapshot.val(),
+  useEffect(() => {
+    firebase.buildings().on('value', snapshot => {
+      setData({
         loading: false,
+        buildings: snapshot.val(),
       });
     });
-  }
+  });
 
-  render() {
-    if (this.state.loading) {
-      return (
-        <Layout>
-          <Center>
-            <CircularProgress />
-          </Center>
-        </Layout>
-      );
-    }
-
-    return (
-      <Layout>
-        {Object.keys(this.state.buildings).map(buildingId => (
-          <Building id={buildingId} key={buildingId} />
-        ))}
-      </Layout>
-    );
-  }
+  return buildingData.loading ? (
+    <Layout>
+      <Center>
+        <CircularProgress />
+      </Center>
+    </Layout>
+  ) : (
+    <Layout>
+      {Object.keys(buildingData.buildings).map(buildingId => (
+        <Building id={buildingId} key={buildingId} />
+      ))}
+    </Layout>
+  );
 }
 
 export default withFirebase(Buildings);
